9eacdbabb19c54da8bd9936e022a49ff6484e141
Elisa <elisabzh10@gmail.com>
2025-03-05 13:32:40 +0100

    Revert "utilisateurentity comments"
    This reverts commit 96e6f7dc08941ea9d50332d5f6ad29e7ed41f55c.

--- helloword-vscode/src/main/java/es/codeurjc/helloword_vscode/entities/UtilisateurEntity.java
+++ helloword-vscode/src/main/java/es/codeurjc/helloword_vscode/entities/UtilisateurEntity.java
@@ -8,37 +8,29 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import java.util.List;
+
 import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
 import jakarta.persistence.JoinColumn;
 
-@Entity // Marks the class as a JPA entity, to be mapped to a table in the database
+@Entity
 public class UtilisateurEntity {
-
-    @Id // Specifies the primary key field for the UtilisateurEntity
-    @GeneratedValue(strategy = GenerationType.AUTO) // Automatically generates the value for the id field
-    long id;
-
-    // Name and surname of the user
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	long id;
     private String name;
     private String surname;
-    
-    // User's password (it might be hashed for security purposes)
     private String pwd;
 
-    // List of roles assigned to the user, using an element collection (e.g., "Admin", "Member", etc.)
-    @ElementCollection(fetch = FetchType.EAGER) // Elements are eagerly fetched when the entity is loaded
-    private List<String> roles;
+    @ElementCollection(fetch = FetchType.EAGER)
+	private List<String> roles;
 
-    // One-to-many relationship with the MemberType entity
-    // This indicates that a user can have multiple member types (roles) in different associations
-    @OneToMany(mappedBy = "Utilisateurentity") 
+    @OneToMany(mappedBy = "Utilisateurentity")
     private List<MemberType> memberTypes;
 
-    // Default constructor required by JPA for entity initialization
+    // Constructor
     public UtilisateurEntity() {}
 
-    // Constructor to initialize a user entity with specific values for name, surname, password, and roles
     public UtilisateurEntity(String name, String surname, String pwd, String... roles) {
         this.name = name;
         this.surname = surname;
@@ -46,7 +38,7 @@ public class UtilisateurEntity {
         this.roles = List.of(roles);
     }
 
-    // Getters and Setters for all attributes
+    // Getters and Setters
     public long getId() {
         return id;
     }
@@ -79,12 +71,11 @@ public class UtilisateurEntity {
         this.pwd = pwd;
     }
 
-    // Getter and Setter for the roles assigned to the user
-    public List<String> getRoles() {
-        return roles;
-    }
+	public List<String> getRoles() {
+		return roles;
+	}
 
-    public void setRoles(List<String> roles) {
-        this.roles = roles;
-    }
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
+	}
 }
