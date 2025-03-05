d667af2530266f440a700754eea461dd3209afce
Elisa <elisabzh10@gmail.com>
2025-03-05 13:32:00 +0100

    Revert "utilentiserv comments"
    This reverts commit ba5fed9a4d73edda77b6a334086033f8cb3556f3.

--- helloword-vscode/src/main/java/es/codeurjc/helloword_vscode/service/UtilisateurEntityService.java
+++ helloword-vscode/src/main/java/es/codeurjc/helloword_vscode/service/UtilisateurEntityService.java
@@ -1,6 +1,5 @@
 package es.codeurjc.helloword_vscode.service;
 
-// Importing necessary classes for Spring Security and repository management
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,27 +14,25 @@ import org.springframework.stereotype.Service;
 import es.codeurjc.helloword_vscode.entities.UtilisateurEntity;
 import es.codeurjc.helloword_vscode.repository.UtilisateurEntityRepository;
 
-@Service // Marks this class as a Spring service that provides user details for authentication
+@Service
 public class UtilisateurEntityService implements UserDetailsService {
 
-    @Autowired // Automatically injects the UtilisateurEntityRepository to handle user data access
+    @Autowired
 	private UtilisateurEntityRepository UtilisateursEntityRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // Finds the user by username or throws an exception if the user doesn't exist
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
 		UtilisateurEntity utilisateur = UtilisateursEntityRepository.findByName(username)
 				.orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
-		// Creates a list of roles for the user to use in authentication
 		List<GrantedAuthority> roles = new ArrayList<>();
 		for (String role : utilisateur.getRoles()) {
-			// Adding "ROLE_" prefix to each role for Spring Security format
 			roles.add(new SimpleGrantedAuthority("ROLE_" + role));
 		}
 
-		// Returns a new UserDetails object with the user's name, password, and roles
 		return new org.springframework.security.core.userdetails.User(utilisateur.getName(), 
 				utilisateur.getPwd(), roles);
+
 	}
 }
