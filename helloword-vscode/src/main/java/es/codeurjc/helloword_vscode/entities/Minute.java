b3e6a805eb73952e04cae12a051aece1a89b8daa
Elisa <elisabzh10@gmail.com>
2025-03-05 13:32:51 +0100

    Revert "minute comments"
    This reverts commit ea082d30c10d655ab9a0baee369c228e2567e3ea.

--- helloword-vscode/src/main/java/es/codeurjc/helloword_vscode/entities/Minute.java
+++ helloword-vscode/src/main/java/es/codeurjc/helloword_vscode/entities/Minute.java
@@ -1,6 +1,7 @@
 package es.codeurjc.helloword_vscode.entities;
 
 import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,38 +11,31 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.JoinTable;
 
-@Entity // Marks the class as a JPA entity, which will be mapped to a table in the database
+@Entity
 public class Minute {
 
-    @Id // Specifies the primary key field for the Minute entity
-    @GeneratedValue(strategy = GenerationType.AUTO) // Automatically generates the value for the id field
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    // The date of the meeting
     private String date;
 
-    // Many-to-many relationship with UtilisateurEntity (users/participants of the meeting)
-    // This means many users can attend multiple meetings, and each meeting can have multiple users
     @ManyToMany
     @JoinTable(
-        name = "minute_participants", // The name of the join table that links minutes and participants
-        joinColumns = @JoinColumn(name = "minute_id"), // The column that refers to the minute entity
-        inverseJoinColumns = @JoinColumn(name = "Utilisateur_id") // The column that refers to the participant (UtilisateurEntity)
+        name = "minute_participants",
+        joinColumns = @JoinColumn(name = "minute_id"),
+        inverseJoinColumns = @JoinColumn(name = "Utilisateur_id")
     )
     private List<UtilisateurEntity> participants;
 
-    // Content of the meeting (the minutes written during the meeting)
     private String content;
-
-    // Duration of the meeting in hours (e.g., 1.5 hours)
     private double duration;
 
-    // Many-to-one relationship with the Association entity (the association that organized the meeting)
     @ManyToOne
-    @JoinColumn(name = "idAssociation") // Foreign key column linking to the Association entity
+    @JoinColumn(name = "idAssociation")
     private Association association;
 
-    // Constructor to initialize the Minute entity with specific values for its attributes
+    // Constructeur
     public Minute(String date, List<UtilisateurEntity> participants, String content, double duration, Association association) {
         this.date = date;
         this.participants = participants;
@@ -50,10 +44,10 @@ public class Minute {
         this.association = association;
     }
 
-    // Default constructor required by JPA for entity initialization
+    // Constructeur par défaut
     public Minute() {}
 
-    // Getters and Setters for all attributes
+    // Getters et Setters
     public long getId() {
         return id;
     }
