8d0243c91841524a6c5cdd81c997e58144598041
Elisa <elisabzh10@gmail.com>
2025-03-05 13:32:17 +0100

    Revert "dataInit"
    This reverts commit 774a22b733a9ed4162b3f50b8ca60ed1d19e9b19.

--- helloword-vscode/src/main/java/es/codeurjc/helloword_vscode/service/DataInitializer.java
+++ helloword-vscode/src/main/java/es/codeurjc/helloword_vscode/service/DataInitializer.java
@@ -1,8 +1,8 @@
 package es.codeurjc.helloword_vscode.service;
 
-// Importing necessary classes for Spring Data, Spring Security, and data initialization
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import es.codeurjc.helloword_vscode.entities.Association;
 import es.codeurjc.helloword_vscode.entities.Minute;
 import es.codeurjc.helloword_vscode.entities.MemberType;
@@ -11,15 +11,17 @@ import es.codeurjc.helloword_vscode.repository.AssociationRepository;
 import es.codeurjc.helloword_vscode.repository.MinuteRepository;
 import es.codeurjc.helloword_vscode.repository.MemberTypeRepository;
 import es.codeurjc.helloword_vscode.repository.UtilisateurEntityRepository;
-import jakarta.annotation.PostConstruct; // Used to mark the method to be executed after bean initialization
+
+import jakarta.annotation.PostConstruct;
 import java.util.Arrays;
-import org.springframework.security.crypto.password.PasswordEncoder; // Used for password encoding
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-@Service // Marks this class as a Spring service component
+@Service
 public class DataInitializer {
 
-    @Autowired // Automatically injects the dependencies for repository beans
+    @Autowired
     private UtilisateurEntityRepository UtilisateurEntityRepository;
 
     @Autowired
@@ -32,34 +34,35 @@ public class DataInitializer {
     private AssociationRepository associationRepository;
 
     @Autowired
-    private PasswordEncoder passwordEncoder; // Password encoder to securely store passwords
+    private PasswordEncoder passwordEncoder;
 
-    @PostConstruct // This method is executed after the bean has been initialized
+    @PostConstruct
     public void init() {
-        // Add users with encoded passwords
+        // Add users
         UtilisateurEntity Utilisateurentity1 = new UtilisateurEntity("Jean", "Jan", passwordEncoder.encode("mdp"), "USER");
         UtilisateurEntity Utilisateurentity2 = new UtilisateurEntity("Pierre", "Pro", passwordEncoder.encode("pwd"), "USER", "ADMIN");
-        // Save users to the database
         UtilisateurEntityRepository.saveAll(Arrays.asList(Utilisateurentity1, Utilisateurentity2));
 
         // Add associations
         Association association1 = new Association("GreenPeace");
+        associationRepository.save(association1);
         Association association2 = new Association("GreatSchool");
+        associationRepository.save(association2);
         Association association3 = new Association("Help");
-        // Save associations to the database
-        associationRepository.saveAll(Arrays.asList(association1, association2, association3));
+        associationRepository.save(association3);
 
-        // Add roles (member types)
+        // Add roles
         MemberType role1 = new MemberType("secretary", Utilisateurentity1, association1);
+        roleRepository.save(role1);
         MemberType role2 = new MemberType("president", Utilisateurentity2, association1);
+        roleRepository.save(role2);
         MemberType role3 = new MemberType("president", Utilisateurentity2, association2);
-        // Save roles to the database
-        roleRepository.saveAll(Arrays.asList(role1, role2, role3));
+        roleRepository.save(role3);
 
-        // Add minutes (meeting records)
-        Minute minute1 = new Minute("2023-10-01", Arrays.asList(Utilisateurentity1, Utilisateurentity2), "New actions about climate", 60.0, association1);
+        // Add minutes
+        Minute minute1 = new Minute("2023-10-01", Arrays.asList(Utilisateurentity1, Utilisateurentity2), "New actions about climat", 60.0, association1);
+        minuteRepository.save(minute1);
         Minute minute2 = new Minute("2024-11-03", Arrays.asList(Utilisateurentity1, Utilisateurentity2), "Discussion on government measures", 30.0, association1);
-        // Save minutes to the database
-        minuteRepository.saveAll(Arrays.asList(minute1, minute2));
+        minuteRepository.save(minute2);
     }
 }
