e11492eeed796dd27a3cdfe684be1bd49efca510
Elisa <elisabzh10@gmail.com>
2025-03-05 13:32:56 +0100

    Revert "membertype comments"
    This reverts commit be03b221f7fc4af7373273f996cd7b74c77ac257.

--- helloword-vscode/src/main/java/es/codeurjc/helloword_vscode/entities/MemberType.java
+++ helloword-vscode/src/main/java/es/codeurjc/helloword_vscode/entities/MemberType.java
@@ -8,39 +8,34 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.CascadeType;
 
-@Entity // Marks the class as a JPA entity, to be mapped to a table in the database
+@Entity
 public class MemberType {
 
-    @Id // Specifies the primary key field
-    @GeneratedValue(strategy = GenerationType.AUTO) // Auto-generates the id value
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    // The name of the member type (e.g., "President", "Secretary")
     private String name;
 
-    // Many-to-one relationship with UtilisateurEntity (user entity)
-    // This means a member type is associated with one user, but multiple member types can exist for different users
-    @ManyToOne(cascade = CascadeType.REMOVE) // Cascade remove action ensures that when a member type is removed, the associated user is also deleted if no longer in use
-    @JoinColumn(name = "idUtilisateur") // Foreign key linking to the 'UtilisateurEntity' table
-    private UtilisateurEntity utilisateurEntity;
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "idUtilisateur")
+    private UtilisateurEntity Utilisateurentity;
 
-    // Many-to-one relationship with Association entity
-    // This means a member type is linked to one association, but an association can have many member types
-    @ManyToOne(cascade = CascadeType.REMOVE) // Cascade remove action ensures that when a member type is removed, the association's relation is also updated
-    @JoinColumn(name = "idAssociation") // Foreign key linking to the 'Association' table
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "idAssociation")
     private Association association;
 
-    // Default constructor required by JPA for entity initialization
+    // Constructeur par défaut
     public MemberType() {}
 
-    // Constructor to initialize a member type with its name, associated user, and association
-    public MemberType(String name, UtilisateurEntity utilisateurEntity, Association association) {
+    // Constructeur avec paramètres
+    public MemberType(String name, UtilisateurEntity Utilisateurentity, Association association) {
         this.name = name;
-        this.utilisateurEntity = utilisateurEntity;
+        this.Utilisateurentity = Utilisateurentity;
         this.association = association;
     }
 
-    // Getters and Setters
+    // Getters et Setters
     public long getId() {
         return id;
     }
@@ -57,16 +52,14 @@ public class MemberType {
         this.name = name;
     }
 
-    // Getter and Setter for the associated user
     public UtilisateurEntity getUtilisateurEntity() {
-        return utilisateurEntity;
+        return Utilisateurentity;
     }
 
-    public void setUtilisateurEntity(UtilisateurEntity utilisateurEntity) {
-        this.utilisateurEntity = utilisateurEntity;
+    public void setUtilisateurEntity(UtilisateurEntity Utilisateurentity) {
+        this.Utilisateurentity = Utilisateurentity;
     }
 
-    // Getter and Setter for the associated association
     public Association getAssociation() {
         return association;
     }
