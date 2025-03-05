1a93797c03809936d5eccc50d9cde9e2f1ade1cd
Elisa <elisabzh10@gmail.com>
2025-03-05 13:32:10 +0100

    Revert "minute and role service ud"
    This reverts commit c8a51279de3991c07d9d887b73b608ad6ef6ea1b.

--- helloword-vscode/src/main/java/es/codeurjc/helloword_vscode/service/RoleService.java
+++ helloword-vscode/src/main/java/es/codeurjc/helloword_vscode/service/RoleService.java
@@ -1,14 +1,12 @@
 package es.codeurjc.helloword_vscode.service;
 
-// Importing necessary classes for Spring service and repository management
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import es.codeurjc.helloword_vscode.repository.MemberTypeRepository;
 
-@Service // Marks this class as a Spring service where business logic for role-related operations will be implemented
+@Service
 public class RoleService {
-    
-    @Autowired // Automatically injects the MemberTypeRepository to handle role-related database operations
-    private MemberTypeRepository RolesRepository; // Repository that provides access to the MemberType entity (roles)
+    @Autowired
+	private MemberTypeRepository RolesRepository;
 }
