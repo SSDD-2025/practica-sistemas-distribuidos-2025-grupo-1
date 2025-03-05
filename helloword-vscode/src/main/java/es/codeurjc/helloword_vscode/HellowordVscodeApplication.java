762b095c98c8fb99a6d5d83dc806cd4ff2e9cad5
Elisa <elisabzh10@gmail.com>
2025-03-05 13:31:47 +0100

    Revert "main comments"
    This reverts commit 7e46e82f4d416ca4e2772bce5ab1759e28d86f70.

--- helloword-vscode/src/main/java/es/codeurjc/helloword_vscode/HellowordVscodeApplication.java
+++ helloword-vscode/src/main/java/es/codeurjc/helloword_vscode/HellowordVscodeApplication.java
@@ -1,16 +1,13 @@
 package es.codeurjc.helloword_vscode;
 
-// Importing Spring Boot classes for bootstrapping the application
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication // Marks this class as the main entry point for the Spring Boot application
+@SpringBootApplication
 public class HellowordVscodeApplication {
 
-    // Main method that launches the Spring Boot application
-    public static void main(String[] args) {
-        // Runs the Spring application with the specified class and command line arguments
-        SpringApplication.run(HellowordVscodeApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(HellowordVscodeApplication.class, args);
+	}
 
 }
